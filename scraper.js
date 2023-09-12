23da64f4c5dd510b82d7f9e510f0c7488b23ade5
MoroniStubbe <moronistubbe2000@gmail.com>
2023-09-12 11:57:38 +0200

    remove selectFromArray

--- scraper.js
+++ scraper.js
@@ -1,26 +1,3 @@
-function selectFromArray(list, selector) {
-	const selections = selector.split(',').map(item => item.trim());
-	const selectedItems = [];
-
-	selections.forEach(selection => {
-		if (selection.includes('-')) {
-			const [start, end] = selection.split('-').map(Number);
-			for (let i = start; i <= end; i++) {
-				if (i >= 0 && i < list.length) {
-					selectedItems.push(list[i]);
-				}
-			}
-		} else {
-			const index = Number(selection);
-			if (index >= 0 && index < list.length) {
-				selectedItems.push(list[index]);
-			}
-		}
-	});
-
-	return selectedItems;
-}
-
 function selectElements(selector) {
 	let elements = [document];
 	let elements2;
