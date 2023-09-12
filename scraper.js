a70354bcc65f7cc3d27d09c2b3a7d9168b02f466
MoroniStubbe <moronistubbe2000@gmail.com>
2023-09-12 22:02:04 +0200

    add getPath

--- scraper.js
+++ scraper.js
@@ -32,24 +32,58 @@ function el(path) {
 }
 
 function getUniqueClasses() {
-	// Get all elements with class attributes
 	const elementsWithClasses = document.querySelectorAll("[class]");
-
-	// Create an object to store class occurrences
 	const classOccurrences = {};
-
-	// Iterate through the elements and count class occurrences
 	elementsWithClasses.forEach((element) => {
 		const classes = element.getAttribute("class").split(" ");
 		classes.forEach((className) => {
 			classOccurrences[className] = (classOccurrences[className] || 0) + 1;
 		});
 	});
-
-	// Filter class names that occur only once
 	const uniqueClasses = Object.keys(classOccurrences).filter(
 		(className) => classOccurrences[className] === 1
 	);
-
 	return uniqueClasses;
+}
+
+function getPath(id) {
+	const uniqueClasses = getUniqueClasses();
+	let path = '';
+	let done = false;
+	let element = el(id);
+	let skipIdCheck = true;
+	while (!done) {
+		if (!skipIdCheck && element.id) {
+			path = element.id + ' ' + path;
+			break;
+		}
+		skipIdCheck = false;
+		let classes = element.getAttribute('class');
+		if (classes) {
+			classes = classes.split(' ');
+			for (const className of classes) {
+				if (uniqueClasses.includes(className)) {
+					done = true;
+					path = '.' + className + ' ' + path;
+					break;
+				}
+			}
+		}
+		if (done) {
+			break;
+		}
+		const siblings = Array.from(element.parentElement.children).filter((child) => child.tagName === element.tagName);
+		index = siblings.indexOf(element);
+		let tagName = element.tagName.toLowerCase();
+		if (index > 0) {
+			tagName += (index + 1).toString();
+		}
+		path = tagName + ' ' + path;
+		element = element.parentElement;
+		if (element.tagName === 'HTML') {
+			done = true;
+		}
+	}
+	path = path.slice(0, -1);
+	return path;
 }
